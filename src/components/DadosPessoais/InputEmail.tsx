e32538374a159a0ac7c41297a9d30eb400c1c792
LaBabaquita <101640412+LaBabaquita@users.noreply.github.com>
2022-09-20 18:22:04 -0300

    Add files via upload

---
+++ src/components/DadosPessoais/InputEmail.tsx
@@ -0,0 +1,6 @@
+export default function() {
+    return <div className="container-email">
+        <label htmlFor="Email">Email:</label>
+        <input id="Email"></input>
+    </div>
+}
---
+++ src/components/DadosPessoais/InputNomeCompleto.tsx
@@ -0,0 +1,6 @@
+export default function() {
+    return <div className="container-nome">
+        <label htmlFor="nome">Nome Completo:</label>
+        <input id="nome"></input>
+    </div>
+}
